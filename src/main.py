02930d2d2029ed077fc14bfc822e2205a7ac72ce
Shishir Shetty <programmingwithshishir@gmail.com>
2025-05-17 17:30:13 +0530

    Add filtering feature

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from playwright.sync_api import sync_playwright, TimeoutError
+import time
 from config import (
     logger,
+    FILTER_BUTTON_XPATH,
+    LOADER_XPATH,
     EMAIL_XPATH,
     NEXT_BUTTON_XPATH,
     PASSWORD_XPATH,
@@ -48,6 +51,21 @@ def login(page):
             logger.critical("KeyboardInterrupt: Exiting!")
             exit(0)
 
+def filter(page):
+    # Find the filter button
+    filter_button = page.locator(FILTER_BUTTON_XPATH)
+    filter_button.wait_for(state="attached", timeout=TIMEOUT)
+    filter_button.click()
+
+    # Finding filter options
+    filter_options = filter_button.get_by_role("listbox").get_by_role("option").all()
+    for option in filter_options:
+        # TODO: Change this to "Default" or "All (ignore preferences)" depending on testing or not
+        if option.inner_text() == "All (ignore preferences)":
+            option.click()
+            break
+    page.locator(LOADER_XPATH).wait_for(state="hidden", timeout=TIMEOUT)
+
 def main():
     # Launch Playwright browser
     with sync_playwright() as p:
@@ -57,6 +75,10 @@ def main():
         logger.info("Attempting to Login.")
         login(page)
         logger.info("Login successful.")
+        logger.info("Filtering the desired jobs.")
+        filter(page)
+        time.sleep(5)
+        logger.info("Desired jobs filtered.")
 
 if __name__ == "__main__":
     main()
