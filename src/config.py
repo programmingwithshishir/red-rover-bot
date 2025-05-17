2ba74b64cfcabfb6fc808ecfadef71aee4efb8d1
Shishir Shetty <programmingwithshishir@gmail.com>
2025-05-17 16:01:29 +0530

    Add a config file

---
+++ src/config.py
@@ -0,0 +1,34 @@
+import os
+import logging
+from typing import Final
+from dotenv import load_dotenv
+
+load_dotenv()
+REFRESH_SLEEP_TIME: Final = int(os.getenv("REFRESH_SLEEP_TIME"))
+TIMEOUT: Final = int(os.getenv("TIMEOUT")) * 1000
+
+# Pushover Credentials
+PUSHOVER_TOKEN: Final = os.getenv("PUSHOVER_TOKEN")
+PUSHOVER_USER_KEY: Final = os.getenv("PUSHOVER_USER_KEY")
+
+# Red Rover Credentials
+BASE_URL: Final = "https://app.redroverk12.com/substitute"
+CLIENT_EMAIL: Final = os.getenv("REDROVER_EMAIL")
+CLIENT_PASSWORD: Final = os.getenv("REDROVER_PASSWORD")
+
+# Configure Logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger()
+
+# Login XPaths
+EMAIL_XPATH: Final = 'xpath=//*[@id="loginId"]'
+NEXT_BUTTON_XPATH: Final = "xpath=/html/body/main/main/div/div/div/main/form/div/button"
+PASSWORD_XPATH: Final = 'xpath=//*[@id="password"]'
+LOGIN_XPATH: Final = "xpath=/html/body/main/main/div/div/div/main/form/div/button"
+
+# Job XPaths
+FILTER_BUTTON_XPATH: Final = 'xpath=//*[@id="content-view"]/div/div[2]/div[2]/div[2]/div/div/div'
+LOADER_XPATH: Final = 'xpath=//*[@id="edlumin-app"]/div/div/div/div/header/span'
+REFRESH_BUTTON_XPATH: Final = 'xpath=//*[@id="content-view"]/div/div[2]/div[1]/div/button'
+ALL_JOBS_XPATH: Final = 'xpath=//*[@id="content-view"]/div/div[2]/div[3]'
+NOTE_XPATH: Final = "xpath=/html/body/div[5]"
